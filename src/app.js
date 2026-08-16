e57500e86b794d1bd474dc4696ea87c61b7bbcd6
Parth Tomar <parthtomar1019@gmail.com>
2026-08-16 13:18:33 +0530

    make basic routing setup

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.json({limit:'10kb'}))
 app.use(express.urlencoded({extended: true, limit: '10kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//rotues declaration 
+app.use("/api/v1/users", userRouter) //prefix of /users and then the /register
 export {app} 
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,14 @@
+import {asyncHandler} from '../utils/asyncHandler.js'
+
+const registerUser = asyncHandler( async(req, res)=>{
+    // get user details from frontend 
+})
+
+export {registerUser}
+
+// ask for email
+// check if alreayd registred
+// ask for name
+// ask for other details 
+// ask for password
+// uplload on database and redirect to login page 
