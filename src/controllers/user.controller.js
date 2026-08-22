82ff808836c72fb5d588b9e7cf92e781265e6e48
Parth Tomar <parthtomar1019@gmail.com>
2026-08-22 16:39:58 +0530

    add getWatchHistory endpoint to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -395,7 +395,58 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
                     )
 })
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentUserPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage}
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project:{
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                       $addFields:{
+                        owner:{
+                            $first:"$owner"
+                        }
+                       } 
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200)
+                    .json(
+                        new ApiResponse(200, user[0].watchHistory,"WatchHistory fetched successfully")
+                    )
+})
+
+
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentUserPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage, getWatchHistory}
 
 //get email from frontend 
 // check if user registred or not 
